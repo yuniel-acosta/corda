db153d815794575ced74005f380e7c380584ded7
Mike Hearn <mike@r3.com>
2019-05-07 18:54:06 +0200

    Serialisation: use the same whitelist for storage and rpc contexts.

--- serialization/src/main/kotlin/net/corda/serialization/internal/ServerContexts.kt
+++ serialization/src/main/kotlin/net/corda/serialization/internal/ServerContexts.kt
@@ -12,16 +12,14 @@ import net.corda.serialization.internal.amqp.amqpMagic
  * These have been refactored into a separate file to prevent
  * clients from trying to instantiate any of them.
  *
- * NOTE: The [KRYO_STORAGE_CONTEXT] and [AMQP_STORAGE_CONTEXT]
- * CANNOT always be instantiated outside of the server and so
- * MUST be kept separate!
+ * NOTE: The AMQP_STORAGE_CONTEXT *cannot* always be instantiated outside of the server and so *must* be kept separate!
  */
 
 
 val AMQP_STORAGE_CONTEXT = SerializationContextImpl(
         amqpMagic,
         SerializationDefaults.javaClass.classLoader,
-        AllButBlacklisted,
+        GlobalTransientClassWhiteList(BuiltInExceptionsWhitelist()),
         emptyMap(),
         true,
         SerializationContext.UseCase.Storage,
