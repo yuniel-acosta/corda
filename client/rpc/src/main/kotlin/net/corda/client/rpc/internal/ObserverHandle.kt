0461d6ea177c7c2a82d34d8e6bc27da3c9a0e747
Dimos Raptis <dimos.raptis@r3.com>
2019-07-02 13:10:17 +0100

    Remove the need for threadpool

--- client/rpc/src/main/kotlin/net/corda/client/rpc/internal/ObserverHandle.kt
+++ client/rpc/src/main/kotlin/net/corda/client/rpc/internal/ObserverHandle.kt
@@ -1,23 +1,17 @@
 package net.corda.client.rpc.internal
 
-import net.corda.core.utilities.minutes
-import java.time.Duration
-import java.util.*
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.TimeUnit
+import rx.Subscription
 
 /**
  * Handle to externally control a subscribed observer for [ReconnectingObservable]s.
  */
 class ObserverHandle {
-    private val terminated = LinkedBlockingQueue<Optional<Throwable>>(1)
+    var stopped = false
+    var activeSubscription: Subscription? = null
 
-    fun stop() = terminated.put(Optional.empty())
+    fun stop() {
+        activeSubscription?.unsubscribe()
+        stopped = true
+    }
 
-    fun fail(e: Throwable) = terminated.put(Optional.of(e))
-
-    /**
-     * Returns null if the observation ended successfully.
-     */
-    fun await(duration: Duration = 60.minutes): Throwable? = terminated.poll(duration.seconds, TimeUnit.SECONDS).orElse(null)
 }
--- client/rpc/src/main/kotlin/net/corda/client/rpc/internal/ReconnectingObservableImpl.kt
+++ client/rpc/src/main/kotlin/net/corda/client/rpc/internal/ReconnectingObservableImpl.kt
@@ -1,10 +1,12 @@
 package net.corda.client.rpc.internal
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import net.corda.core.messaging.DataFeed
 import net.corda.core.utilities.contextLogger
 import net.corda.core.utilities.debug
 import rx.Observable
 import rx.Subscription
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ExecutorService
 
 class ReconnectingObservableImpl<T> internal constructor(
@@ -18,10 +20,13 @@ class ReconnectingObservableImpl<T> internal constructor(
     constructor(reconnectingRPCConnection: ReconnectingCordaRPCOps.ReconnectingRPCConnection, observersPool: ExecutorService, initial: DataFeed<*, T>, createDataFeed: () -> DataFeed<*, T>):
             this(ReconnectingSubscriber(reconnectingRPCConnection, observersPool, initial, createDataFeed))
 
-    class ReconnectingSubscriber<T>(private val reconnectingRPCConnection: ReconnectingCordaRPCOps.ReconnectingRPCConnection,
-                                    private val observersPool: ExecutorService,
+    class ReconnectingSubscriber<T>(val reconnectingRPCConnection: ReconnectingCordaRPCOps.ReconnectingRPCConnection,
+                                    val observersPool: ExecutorService,
                                     val initial: DataFeed<*, T>,
                                     val createDataFeed: () -> DataFeed<*, T>): OnSubscribe<T>, ReconnectingObservable<T> {
+
+        private val activeSubscriptions = ConcurrentHashMap<ObserverHandle, Subscription>()
+
         override fun call(child: rx.Subscriber<in T>) {
             val handle = subscribe(child::onNext, {}, {}, {})
             // this additional subscription allows us to detect un-subscription calls from clients and un-subscribe any subscription that resulted from re-connections.
@@ -42,39 +47,33 @@ class ReconnectingObservableImpl<T> internal constructor(
 
         private var initialStartWith: Iterable<T>? = null
         private fun subscribeWithReconnect(observerHandle: ObserverHandle, onNext: (T) -> Unit, onStop: () -> Unit, onDisconnect: () -> Unit, onReconnect: () -> Unit, startWithValues: Iterable<T>? = null) {
-            var subscriptionError: Throwable?
             try {
                 val subscription = initial.updates.let { if (startWithValues != null) it.startWith(startWithValues) else it }
-                        .subscribe(onNext, observerHandle::fail, observerHandle::stop)
-                subscriptionError = observerHandle.await()
-                subscription.unsubscribe()
+                        .subscribe(onNext,
+                                { error ->
+                                    activeSubscriptions[observerHandle]!!.unsubscribe()
+                                    onDisconnect()
+                                    reconnectingRPCConnection.error(error)
+                                    log.debug { "Recreating data feed." }
+                                    val newObservable = createDataFeed().updates as ReconnectingObservableImpl<T>
+                                    onReconnect()
+                                    if (!observerHandle.stopped)
+                                        newObservable.reconnectingSubscriber.subscribeWithReconnect(observerHandle, onNext, onStop, onDisconnect, onReconnect, startWithValues)
+                                },
+                                {
+                                    onStop()
+                                    activeSubscriptions[observerHandle]!!.unsubscribe()
+                                })
+                activeSubscriptions[observerHandle] = subscription
+                observerHandle.activeSubscription = subscription
             } catch (e: Exception) {
                 log.error("Failed to register subscriber .", e)
-                subscriptionError = e
             }
-
-            // In case there was no exception the observer has finished gracefully.
-            if (subscriptionError == null) {
-                onStop()
-                return
-            }
-
-            onDisconnect()
-            // Only continue if the subscription failed.
-            reconnectingRPCConnection.error(subscriptionError)
-            log.debug { "Recreating data feed." }
-
-            val newObservable = createDataFeed().updates as ReconnectingObservableImpl<T>
-            onReconnect()
-            return newObservable.reconnectingSubscriber.subscribeWithReconnect(observerHandle, onNext, onStop, onDisconnect, onReconnect)
         }
 
         override fun subscribe(onNext: (T) -> Unit, onStop: () -> Unit, onDisconnect: () -> Unit, onReconnect: () -> Unit): ObserverHandle {
             val observerNotifier = ObserverHandle()
-            // TODO - change the establish connection method to be non-blocking
-            observersPool.execute {
-                subscribeWithReconnect(observerNotifier, onNext, onStop, onDisconnect, onReconnect, initialStartWith)
-            }
+            subscribeWithReconnect(observerNotifier, onNext, onStop, onDisconnect, onReconnect, initialStartWith)
             return observerNotifier
         }
 
