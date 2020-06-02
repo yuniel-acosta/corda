e8233a65d615600a71ee65b8aec34e4d5615cadc
tubibuto <antebuterintubi@hotmail.com>
2020-06-02 19:28:29 +0100

    Added DB migration scripts

--- business-networks/contracts/src/main/kotlin/net/corda/bn/schemas/MembershipStateSchemaV1.kt
+++ business-networks/contracts/src/main/kotlin/net/corda/bn/schemas/MembershipStateSchemaV1.kt
@@ -5,11 +5,21 @@ import net.corda.bn.states.MembershipStatus
 import net.corda.core.identity.Party
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
+import net.corda.core.serialization.CordaSerializable
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
 
+@CordaSerializable
 object MembershipStateSchemaV1 : MappedSchema(schemaFamily = MembershipState::class.java, version = 1, mappedTypes = listOf(PersistentMembershipState::class.java)) {
     @Entity
     @Table(name = "membership_state")
-    class PersistentMembershipState(val cordaIdentity: Party, val networkId: String, val status: MembershipStatus) : PersistentState()
+    class PersistentMembershipState(
+            @Column(name = "corda_identity")
+            val cordaIdentity: Party,
+            @Column(name = "network_id")
+            val networkId: String,
+            @Column(name = "status")
+            val status: MembershipStatus
+    ) : PersistentState()
 }
